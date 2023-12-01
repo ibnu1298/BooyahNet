e9fe599353d4a6da08cd9a7cbe2be4fba02c4cd5
Ibnu Aqil Al Barohin <kalangkang1298@gmail.com>
2023-12-02 01:34:47 +0700

    removegetPackage

--- src/app/package/page.tsx
+++ src/app/package/page.tsx
@@ -12,34 +12,34 @@ interface responsePackages {
   message: string;
   isSucceeded: boolean;
 }
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpYm51YXFpbDEyOThAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE3MDE0NDA3MzF9.t3BJwty76Xcoi-8GE49hYEP1hkcRgEjSZZBSfu5Ehic";
-const url = "https://booyahnetapi.azurewebsites.net/api/Package";
-async function getPackage() {
-  const res = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    cache: "no-cache",
-    next: {
-      tags: ["paket"], //revalidate manual menggunakan api local
-      revalidate: 60, //revalidate setiap 60 detik
-      //jadi bisa tunggu 1 menit atau hit api revalidate dengan tag=paket
-    },
-  });
-  if (!res.ok) {
-    throw new Error("Gagal Fecthing Data");
-  }
+// const token =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpYm51YXFpbDEyOThAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE3MDE0NDA3MzF9.t3BJwty76Xcoi-8GE49hYEP1hkcRgEjSZZBSfu5Ehic";
+// const url = "https://booyahnetapi.azurewebsites.net/api/Package";
+// async function getPackage() {
+//   const res = await fetch(url, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//     cache: "no-cache",
+//     next: {
+//       tags: ["paket"], //revalidate manual menggunakan api local
+//       revalidate: 60, //revalidate setiap 60 detik
+//       //jadi bisa tunggu 1 menit atau hit api revalidate dengan tag=paket
+//     },
+//   });
+//   if (!res.ok) {
+//     throw new Error("Gagal Fecthing Data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 export default async function PackagePage() {
-  const data = await getPackage();
+  // const data = await getPackage();
   return (
     <div className="grid grid-cols-4">
-      {data.packages.length > 0 &&
+      {/* {data.packages.length > 0 &&
         data.packages.map((myPackage: Package) => (
           <div key={myPackage.id}>
             <div>Package Page</div>
@@ -49,7 +49,7 @@ export default async function PackagePage() {
             <div>Max Banwitdh : {myPackage.maxBandwidth}</div>
             <br />
           </div>
-        ))}
+        ))} */}
     </div>
   );
 }
