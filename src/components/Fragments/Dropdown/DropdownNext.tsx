594724c7e16d972767a1e5e196fb8727f8528002
Ibnu Aqil Al Barohin <ibnuaqil.albarohin1298@gmail.com>
2023-12-24 05:55:46 +0700

    addauthProvider

--- src/components/Fragments/Dropdown/DropdownNext.tsx
+++ src/components/Fragments/Dropdown/DropdownNext.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {
   Dropdown,
   DropdownTrigger,
@@ -13,12 +13,20 @@ import Image from "next/image";
 export default function DropdownNext() {
   const { data: session }: { data: any } = useSession();
   console.log(session);
+  let image = "";
+  if (session?.user.image != undefined) {
+    image = session?.user.image as string;
+  }
+
+  console.log(image);
+
   const name =
     session?.user.firstName.charAt(0).toUpperCase() +
     session?.user.firstName.slice(1) +
     " " +
     session?.user.lastName.charAt(0).toUpperCase() +
     session?.user.lastName.slice(1);
+
   return (
     <div className="flex items-center justify-center">
       <Dropdown
@@ -29,24 +37,38 @@ export default function DropdownNext() {
           <div className="flex items-center justify-center cursor-pointer">
             <div className="text-right">
               <div className="text-md text-bold ">{name}</div>
-              <div className="text-xs text-slate-400 -mt-1">
-                {session?.user.email == ""
-                  ? session?.user.userName
-                  : session?.user.email}
-              </div>
+              {name == "" ? (
+                <div className="text-md text-bold ">
+                  {session?.user.email == ""
+                    ? session?.user.userName
+                    : session?.user.email}
+                </div>
+              ) : (
+                <div className="text-xs text-slate-400 -mt-1">
+                  {session?.user.email == ""
+                    ? session?.user.userName
+                    : session?.user.email}
+                </div>
+              )}
             </div>
             <div className="ml-2 hover:text-gray-200 hover:outline outline-offset-2 outline-4 rounded-full outline-slate-600">
-              <Image
-                className="rounded-full w-8 h-8 md:w-10 md:h-10 "
-                src={
-                  session?.user.urlImage != ""
-                    ? session?.user.urlImage
-                    : "/images/people/cat.jpg"
-                }
-                alt={session?.user.name}
-                width={500}
-                height={500}
-              />
+              {image != "" ? (
+                <Image
+                  className="rounded-full w-8 h-8 md:w-10 md:h-10 "
+                  src={image}
+                  width={500}
+                  height={500}
+                  alt={`${session?.user.name}`}
+                />
+              ) : (
+                <Image
+                  className="rounded-full w-8 h-8 md:w-10 md:h-10 "
+                  src="/images/people/cat.jpg"
+                  width={500}
+                  height={500}
+                  alt={`${session?.user.name as string}`}
+                />
+              )}
             </div>
           </div>
         </DropdownTrigger>
